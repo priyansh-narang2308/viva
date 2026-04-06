ecef204ab1fa426ed3e63f79d57bb617c512aace
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-04-06 14:07:36 +0530

    feat: The clerk auth implemented but no change to src

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div>
-      <Button variant="gold" className={"cursor-pointer"} size="hero">
-        Hello
-      </Button>
     </div>
   );
 }
---
+++ src/components/landing/header.jsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { Button } from '../ui/button';
+
+const Header = () => {
+
+    return (
+        <nav className="fixed top-0 inset-x-0 z-50 flex items-center justify-between px-3 sm:px-10 py-3 border-b border-white/7 backdrop-blur-xl">
+            <Link href="/">
+                <Image
+                    src="/log.png"
+                    alt="Viva Logo"
+                    width={150}
+                    height={150}
+                    className="h-15 w-auto"
+                />
+            </Link>
+
+            <div className="flex items-center gap-3">
+                <Show when="signed-out">
+                    <SignInButton mode='modal'>
+                        <Button variant="outline" className={"cursor-pointer"}>Sign In</Button>
+                    </SignInButton>
+                    <SignUpButton mode='modal'>
+                        <Button variant="gold" className={"cursor-pointer"}>Get Started</Button>
+                    </SignUpButton>
+                </Show>
+                <Show when="signed-in">
+                    <UserButton />
+                </Show>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Header
