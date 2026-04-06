e88c86e810b61265cb0f15fb52ff3800da140b5c
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-04-06 19:08:30 +0530

    feat: The main schema of prisma added with all the user tables and transaction tables

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,7 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
 import { defineConfig } from "prisma/config";
+
+dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DIRECT_URL"],
+    url: process.env["DIRECT_URL"] || process.env["DATABASE_URL"],
   },
 });
